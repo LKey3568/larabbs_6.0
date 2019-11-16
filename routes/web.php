a1946defca3d180dc1eea81293dd95879f624321
lkey <lkey_email@163.com>
2019-11-16 02:24:57 +0000

    生产话题骨架

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 // 第四章
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
